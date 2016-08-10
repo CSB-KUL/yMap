11bd4bd146c0f5ae67b0e8834d29369046aca580
AhmedArslan <ahmed.arslan@kuleuven.be>
2016-08-10 12:02:41 +0200

    Add files via upload

---
+++ tests/tests.py
@@ -0,0 +1,18 @@
+import unittest
+from ymap import data, ymap_proteins, ymap_genes, web
+
+class PimpMyPackage(unittest.TestCase):
+	def test_data(self):
+		self.assertTrue(True)
+
+	def test_ymap_proteins(self):
+		self.assertTrue(True)
+
+	def test_ymap_genes(self):
+		self.assertTrue(True)
+
+	def test_web(self):
+		self.assertTrue(True)
+
+if __name__ == '__main__':
+	unittest.main()
