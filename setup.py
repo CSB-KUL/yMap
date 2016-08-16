3633d1dab2b871d502929acc525489278d108f0d
AhmedArslan <ahmed.arslan@kuleuven.be>
2016-08-16 23:55:56 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from distutils.core import setup
+
+
+setup(
+	name = "ymap",
+	
+	version = "1.2.2",
+	
+	description = "An automated method to map yeast variants to protein modifications and functional regions",
+	
+	url = "https://github.com/CSB-KUL/yMap",
+	
+	author = "Ahmed Arslan and Vera van Noort, KU Leuven, Belgium",
+	
+	author_email = "vera.vannoort@kuleuven.be",
+	
+	maintainer = "Ahmed Arslan",
+	
+	maintainer_email = "ahmed.arslan@kuleuven.be",
+	
+	license = "MIT",
+	
+	keywords = (
+		'Biofinformatics',
+		'Proteins variants',
+		'PostTransational-Modifications (PTMs)',
+		'Proteins domains',
+		'Secondary Structures',
+		'Protein-Protein Interactions',
+		'Genomics',
+		'Proteomics',
+		'Gene Ontology',
+		'yMap',
+		),
+
+	classifiers=[ 
+	'Intended Audience :: Science/Research',
+	'Topic :: Education',
+	'License :: OSI Approved :: MIT License',
+	'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    ],
+
+    Package_data = {},
+
+
+	install_requires = ["Orange-Bioinformatics", "Orange"],
+	
+	include_package_data = True,
+	
+	entry_points={
+        'console_scripts': [
+            'ydata=ymap.ymap:data',
+            'ygenes=ymap.ymap:ymap_genes',
+            'yproteins=ymap.ymap:ymap_proteins',
+            'yweb=ymap:web'
+
+        ],
+    }
+)
+
+
