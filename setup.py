aeb91b1124e3addaa06fd3565123cb92cebb6ac2
AhmedArslan <ahmed.arslan@kuleuven.be>
2016-08-17 10:03:13 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -41,14 +41,15 @@ setup(
 	'Topic :: Education',
 	'License :: OSI Approved :: MIT License',
 	'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
+    	'Programming Language :: Python :: 3',
+    	'Programming Language :: Python :: 3.3',
+    	'Programming Language :: Python :: 3.4',
+    	'Programming Language :: Python :: 3.5',
     ],
 
-    Package_data = {},
+    	Package_data = {},
 
+	packages = ['ymap'],
 
 	install_requires = ["Orange-Bioinformatics", "Orange"],
 	
