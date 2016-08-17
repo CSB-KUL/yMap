149adb024594a9d96f2938c541fe2424ed801613
AhmedArslan <ahmed.arslan@kuleuven.be>
2016-08-17 10:03:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 setup(
 	name = "ymap",
 	
-	version = "1.2.2",
+	version = "2.0.1",
 	
 	description = "An automated method to map yeast variants to protein modifications and functional regions",
 	
